51cf47c71f6acb153e2f7f0105c6a8b89f8da1a7
Netra Pradhan <npradha@purdue.edu>
2018-06-17 14:45:29 -0500

    Design aboutMe icons

--- src/About.js
+++ src/About.js
@@ -14,11 +14,76 @@ class About extends Component {
           alt="Netra"
         />
         </div>
-        <div className="content">
+       <div className="content">
         <p className="aboutMe">About Me!</p>
           <div className="aboutContent">
-              <p> ABOUT MEMEMEMEMEMEM
-                </p>
+              {/* <p> MOM IS A BUTT
+                </p> */}
+                <div className="aboutbuttons">
+                  <div className="circle">
+                    <img 
+                      className="tech"
+                      src={require('/Users/netrapradhan/personalWebsite/personal-website/src/imgs/tech.png')}
+                      alt='tech'
+                    />
+                  </div>
+                  <div className="circle">
+                    <img
+                      className="coffee"
+                      src={require('/Users/netrapradhan/personalWebsite/personal-website/src/imgs/coffee.png')} 
+                      alt='coffee'
+                    />
+                  </div>
+                 <div className="circle">
+                   <img
+                      className="fashion"
+                      src={require('/Users/netrapradhan/personalWebsite/personal-website/src/imgs/fashion.png')} 
+                      alt='fashion'
+                    /> 
+                 </div>
+                  <div className="circle">
+                    <img
+                      className="travel"
+                      src={require('/Users/netrapradhan/personalWebsite/personal-website/src/imgs/travel.png')} 
+                      alt='travel'
+                    />
+                  </div>
+                  <div className="circle">
+                    <img
+                      className="code"
+                      src={require('/Users/netrapradhan/personalWebsite/personal-website/src/imgs/code.png')} 
+                      alt='code'
+                    />
+                  </div>
+                  <div className="circle">
+                    <img
+                      className="art"
+                      src={require('/Users/netrapradhan/personalWebsite/personal-website/src/imgs/art.png')} 
+                      alt='art'
+                    />
+                  </div>
+                  <div className="circle">
+                    <img
+                      className="womenTech"
+                      src={require('/Users/netrapradhan/personalWebsite/personal-website/src/imgs/womenTech.png')} 
+                      alt='women in tech'
+                    />
+                  </div>
+                  <div className="circle">
+                    <img
+                      className="gym"
+                      src={require('/Users/netrapradhan/personalWebsite/personal-website/src/imgs/gym.png')} 
+                      alt='gym'
+                    />
+                  </div>
+                  <div className="circle">
+                    <img
+                      className="arkansas"
+                      src={require('/Users/netrapradhan/personalWebsite/personal-website/src/imgs/arkansas.png')} 
+                      alt='arkansas'
+                    />
+                  </div>
+                </div>
           </div> 
         </div>
       
