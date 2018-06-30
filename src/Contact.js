98b9dbba5904acc9af89d6406d60295205b6e858
Netra Pradhan <npradha@purdue.edu>
2018-06-30 00:01:28 -0500

    Add email using FormSpree

--- src/Contact.js
+++ src/Contact.js
@@ -7,12 +7,13 @@ class Contact extends Component {
   render() {
     return (
       <div className="Contact">
-        <form method="post" name="myemailform" action="email.php">
-            Enter Name: <input type="text" name="name"/>
-            Enter Email Address:<input type="text" name="email"/>
-            Enter Message:  <textarea name="message"></textarea>
-        <input type="submit" value="Send Form"/>
-        </form>
+       <form method="POST" action="https://formspree.io/bhs.pradhann@gmail.com">
+        <input type="name" name="nameuser" placeholder="Your name"/>
+        <input type="email" name="email" placeholder="Your email"/>
+        <textarea name="message" placeholder="Your message"></textarea>
+        <input type="hidden" name="_next" value="https://facebook.com" />
+        <button type="submit">Send</button>
+      </form>
 
       </div>
     );
