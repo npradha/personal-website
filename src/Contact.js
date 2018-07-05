6edde5b5c0ff710c522224899a03d6e731a190a7
Netra Pradhan <npradha@purdue.edu>
2018-07-05 12:59:09 -0500

    Add a change to Github pages

--- src/Contact.js
+++ src/Contact.js
@@ -7,10 +7,11 @@ class Contact extends Component {
   render() {
     return (
       <div className="Contact">
+      <p className="title">Let's get in touch!</p>
        <form method="POST" action="https://formspree.io/bhs.pradhann@gmail.com">
-        <input type="name" name="nameuser" placeholder="Your name"/>
-        <input type="email" name="email" placeholder="Your email"/>
-        <textarea name="message" placeholder="Your message"></textarea>
+        <input type="text" name="nameuser" placeholder="Your name"/><br></br>
+        <input type="email" name="email" placeholder="Your email"/><br></br>
+        <textarea name="message" placeholder="Your message"></textarea><br></br>
         <input type="hidden" name="_next" value="https://facebook.com" />
         <button type="submit">Send</button>
       </form>
