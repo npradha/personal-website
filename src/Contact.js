f1951172cd1f9b687568b02c8ea6c90cebdfbd26
Netra Pradhan <npradha@purdue.edu>
2018-08-05 22:05:12 -0500

    Change contact form back to third-party application

--- src/Contact.js
+++ src/Contact.js
@@ -7,7 +7,7 @@ class Contact extends Component {
   render() {
     return (
       <div className="Contact">
-      <p className="title">Let's get in touch!</p>
+      <p className="title">Let's get in touch woot!</p>
       <p className="msgInfo">If you have any questions, comments, or want to schedule a coffee chat, send me a message!</p>
        {/* <form method="POST" action="https://formspree.io/bhs.pradhann@gmail.com">
         <input type="text" name="nameuser" placeholder="Your name"/><br></br>
@@ -19,19 +19,18 @@ class Contact extends Component {
         </button>
       </form> */}
 
-      <form action="email.php" method="POST">
+     <form method="POST" action="https://formspree.io/bhs.pradhann@gmail.com">
         <p>Name</p> 
-          <input type="text" name="name"/>
+        <input type="text" name="nameuser" />
         <p>Email</p> 
-         <input type="text" name="email"/>
+        <input type="email" name="email"/>
         <p>Message</p>
-          <textarea name="message" rows="6" cols="25"></textarea><br />
+        <textarea name="message"></textarea><br />
+        <input type="hidden" name="_next" value="/" />
         <button type="submit">
         <img className="send" src={require('/Users/netrapradhan/personalWebsite/personal-website/src/imgs/send.png')} alt="send"/>
         </button> 
         
-        
-        {/* <input className="send" type="submit" value="Send"/> */}
       </form> 
 
       </div>
